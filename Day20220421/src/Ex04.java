32a92152641b1f1cbac7b704a8b0b9b0197e72c4
Ezen01 <yumkl123@gmail.com>
2022-04-22 11:23:29 +0900

    suwon pet eco1

--- Day20220421/src/Ex04.java
+++ Day20220421/src/Ex04.java
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
+=======
+import java.util.Scanner;
+>>>>>>> a84f3191324750278aa8c7a91b76a99776d5795f
 
 public class Ex04 {
 
 	public static void main(String[] args) {
 		
+<<<<<<< HEAD
 		// 두 배열 요소가 같으면 true, 틀리면 false
 		int[] nArr1 = {10, 20, 3, 40, 50, 5};
 		int[] nArr2 = {10, 20, 3, 40, 50};
@@ -27,4 +32,20 @@ public class Ex04 {
 		System.out.println(isEqual);
 	}
  
+=======
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("두 자리 정수를 입력하세요!");
+		
+		while (true) {
+			int num = scanner.nextInt();
+			
+			if(10 <= num && num <= 99) {
+				System.out.println("두 자리 정수: "+num);
+				break;
+			}
+			System.out.println("두 자리 정수가 아닙니다.");
+		}
+	}
+
+>>>>>>> a84f3191324750278aa8c7a91b76a99776d5795f
 }
